a1c92dc5476403735bc37bcd2c50b10a7f10c773
jaxnalia <jacksendall@gmail.com>
2025-09-11 03:59:33 +0100

    changed og image

--- schizodio-master/app/src/routes/+page.svelte
+++ schizodio-master/app/src/routes/+page.svelte
@@ -9,11 +9,11 @@
 	<meta name="twitter:creator" content="@schizodio">
 	<meta name="twitter:title" content="SCHIZODIO">
 	<meta name="twitter:description" content="Schizodio is bareback sex on Starknet blockchain.">
-	<meta name="twitter:image" content="https://schizodio.xyz/og-image.png">
+	<meta name="twitter:image" content="https://schizodio.xyz/og-large.png">
 	<meta property="og:title" content="SCHIZODIO">
 	<meta property="og:type" content="website">
 	<meta property="og:url" content="https://schizodio.xyz/">
-	<meta property="og:image" content="https://schizodio.xyz/og-image.png">
+	<meta property="og:image" content="https://schizodio.xyz/og-large.png">
 	<meta property="og:description" content="Schizodio is bareback sex on Starknet blockchain.">
 </svelte:head>
 
